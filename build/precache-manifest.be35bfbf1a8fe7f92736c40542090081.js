c4b9978c09b402c117572f12f7dea854971aceaa
Evandro Goncalves <evandrolvg@gmail.com>
2019-12-26 16:18:51 -0300

    Heroku

---
+++ build/precache-manifest.be35bfbf1a8fe7f92736c40542090081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeff4d4b0ddad8ffd27d18412b7cc37b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "619317a50bbe57fa7655",
+    "url": "/static/js/2.9632f93a.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/static/js/2.9632f93a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "955dd6a7d2f4095108e1",
+    "url": "/static/js/main.9224b1dd.chunk.js"
+  },
+  {
+    "revision": "eeb6f55e12771d4830b4",
+    "url": "/static/js/runtime-main.bea94226.js"
+  }
+]);
